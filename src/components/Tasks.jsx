2190eef70447045d96dada2b445edd5d3ab3bfb0
Elpedroso <pedro.henrique514888@gmail.com>
2025-06-01 18:03:05 -0300

    final

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Buttom from "./Buttom";
 
@@ -17,7 +17,8 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}){
                     <li key={task.id} className="flex gap-2">
                         <button 
                             onClick={() => {onTaskClick(task.id)}} 
-                            className={`bg-slate-400 text-white p-2 rounded-md w-full text-left ${task.isCompleted && "line-through"}`}>
+                            className={`bg-slate-400 text-white p-2 rounded-md w-full text-left flex items-center gap-2 ${task.isCompleted && "line-through"}`}>
+                                {task.isCompleted && <CheckIcon/>}
                                 {task.title}
                         </button>
                         <Buttom onClick={() => onSeeDetailsClick(task)} >
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -13,7 +13,7 @@ function TaskPage(){
             <div className="w-screen h-screen bg-slate-500 p-6">
                 <div className="w-[500px] space-y-4 mx-auto">
                     <div className="flex justify-center relative mb-6">
-                        <button onClick={() => onBackClick()} className="absolute top-0 bottom-0 text-slate-100"><ChevronLeftIcon/></button>
+                        <button onClick={() => onBackClick()} className="absolute left-0 top-0 bottom-0 text-slate-100"><ChevronLeftIcon/></button>
                         <h1 className="text-slate-100 text-3xl font-bold text-center">Detalhes da Tarefa</h1>
                     </div>
                     <div className="bg-slate-200 p-4 rounded-md">
